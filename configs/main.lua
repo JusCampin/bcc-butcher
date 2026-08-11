e5720f8eee78b3adee559fc8d5130f6a20f476f5
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-08-11 01:47:01 -0700

    Update main.lua

--- configs/main.lua
+++ configs/main.lua
@@ -1,8 +1,14 @@
 Config = {
     defaultlang = 'en_lang',
-    currency = 0, -- VORP: 0 cash, 1 gold
-    interactionDistance = 2.0,
+
+    development = { enabled = true },
+
     promptControl = 0x760A9C6F, -- G
     promptHoldMs = 500,
-    development = { enabled = true },
+
+    currency = 0, -- VORP: 0 cash, 1 gold
+
+    interactionDistance = 3.0,
+    carriedEntityDistance = 3.0,
+    horseEntityDistance = 10.0,
 }
