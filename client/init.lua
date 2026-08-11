3fbbea68fdc4f7c2ac5c0a1c4655724d13a7137d
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-08-11 01:46:53 -0700

    Update init.lua

--- client/init.lua
+++ client/init.lua
@@ -1,8 +1,10 @@
 Core = exports.vorp_core:GetCore()
+---@type any
 FeatherMenu = exports['feather-menu'].initiate()
 local utils = exports['bcc-utils'].initiate()
 DBG = utils.Debug:Get('bcc-butcher', Config.development.enabled)
 if DBG then DBG:Enable() end
 
+---@type any
 ButcherMenu = nil
 CurrentButcherSite = nil
